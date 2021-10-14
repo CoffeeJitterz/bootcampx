ce3b4c4097058885eb92b30031a3e111a08cc1c1
Josh Millard <josh.millard10@gmail.com>
2021-10-13 20:09:28 -0400

    SQL security updated

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,18 +6,21 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-const input = process.argv[2]
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+const queryString =`
 SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = ${process.argv[2] || 'JUL02'}
+WHERE cohorts.name = $1 || 'JUL02'}
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`
+
+const input = process.argv[2]
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
